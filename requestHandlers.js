559b805ce80b0ca38d10460ace19fc6f3f2e2ad0
dgildea <dgildea@gmail.com>
2013-06-30 22:28:25 +1000

    more stuff

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
   console.log("Request handler 'start' was called.");
+  response.write("Starting..");
 }
 
 function upload() {
   console.log("Request handler 'upload' was called.");
+  response.write("Upload...");
 }
 
 exports.start = start;
